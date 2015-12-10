6b6633d8de19ea025a497a60c0f46b740dfcfa7b
Gustavo Muñoz López <gjmunozl@unal.edu.co>
2015-12-10 01:21:37 -0500

    Update control_movimiento.v

--- control_movimiento.v
+++ control_movimiento.v
@@ -5,11 +5,11 @@ module control_movimiento (rst,sma,clk, R_vertical_1 , R_vertical_2 , R_horizont
 	input [15:0]R_vertical_1;
 	input [15:0]R_vertical_2;
 	input [15:0]R_horizontal_1;
-	input [15:0]R_horizontal_2;
+	input [15:0]R_horizontal_2; 
 	
 	///////////////////entradas de posición manual
 	input [15:0]theta_manual;
-	input [15:0]theta_actual;
+	input [15:0]theta_actual; 
 	input sma; //////////interruptor modo manual/automatico
 	input rst;
 	///////////////////entradas de posición actual	
@@ -17,15 +17,15 @@ module control_movimiento (rst,sma,clk, R_vertical_1 , R_vertical_2 , R_horizont
 	input [15:0]phi_manual;
 	
 	//////////////////Salida para motores en eje theta y phi
-	output reg [1:0] s_out_theta_pos;
-	output reg [1:0] s_out_theta_neg;
-	output reg [1:0] s_out_phi_pos;
-	output reg [1:0] s_out_phi_neg;
+	output reg s_out_theta_pos;
+	output reg s_out_theta_neg;
+	output reg s_out_phi_pos;
+	output reg s_out_phi_neg;
 
 	//inicialización 
-	reg[1:0] shift_motor=2'b00;
-	reg [15:0] error=3'b101;//5
-	reg [15:0] giro=8'b10110100;//180;
+	reg [1:0] shift_motor=2'b00;
+	parameter [2:0] error=3'b101;//5
+	parameter [7:0] giro=8'b10110100;//180;
 	
 	//always @(R_vertical_1||R_vertical_2||R_horizontal_1||R_horizontal_2)begin
 	//always @(theta_manual||phi_manual)
@@ -35,131 +35,130 @@ module control_movimiento (rst,sma,clk, R_vertical_1 , R_vertical_2 , R_horizont
 
 
 		if(rst)begin 
-	shift_motor=2'b00;
-	shift_R=2'b00;
-	error=3'b101;//5
-	giro=8'b10110100;//180;
-
+			s_out_phi_pos=0;
+			s_out_phi_neg=0;	
+			s_out_theta_pos=0;
+			s_out_theta_neg=0;
 		end else begin
 			
-	
-		if(!sma)begin
-		//----------------------//MODO AUTOMATICO//----------------------//	
-			if(shift_motor==2'b00)begin
-			
-				////////////////mover automaticamente motor theta
+		
+			if(!sma)begin
+			//----------------------//MODO AUTOMATICO//----------------------//	
+				if(shift_motor==2'b00)begin
 				
-				//NO mover motor phi
-				s_out_phi_pos=2'b00;
-				s_out_phi_neg=2'b00;
-	
-				if ((R_vertical_1>=(R_vertical_2-error) )&& (R_vertical_1<=(R_vertical_2+error)))begin
-					//NO mover motor theta
-					s_out_theta_neg=2'b00;
-					s_out_theta_pos=2'b00;
-					shift_motor=2'b10;
-				end else begin 
-					if(R_vertical_1>R_vertical_2)begin
-						s_out_theta_pos=2'b01; // movimiento horario vertical
-						s_out_theta_neg=2'b00;
-					end
-					if(R_vertical_1<R_vertical_2)begin
-						s_out_theta_neg=2'b01; // movimiento anti-horario vertical
-						s_out_theta_pos=2'b00;
-					end	
-				end
-				////////////////mover automaticamente motor theta
-				
-			end else begin		
-			
-				////////////////mover automaticamente motor phi
-
-				//NO mover motor theta
-				s_out_theta_pos=2'b00;
-				s_out_theta_neg=2'b00;
-				
-				if (R_horizontal_1>=(R_horizontal_2-error) && (R_horizontal_1<=(R_horizontal_2+error)))begin
-						//NO mover motor phi
-
-						s_out_phi_pos=2'b00;
-						s_out_phi_neg=2'b00;
-						shift_motor=2'b00; 
-				end else begin 
-					if(R_horizontal_1>R_horizontal_2)begin
-						s_out_phi_pos=2'b01; // movimiento horario  horizontal
-						s_out_phi_neg=2'b00; 
-					end	
-					if(R_horizontal_1<R_horizontal_2)begin
-						s_out_phi_neg=2'b01; // movimiento anti-horario horizontal
-						s_out_phi_pos=2'b00;
-					end	
-				end
-				////////////////mover automaticamente motor phi
-			end
-		//----------------------//MODO AUTOMATICO//----------------------//	
-		end else begin
-		//----------------------//MODO MANUAL//----------------------//
-			
-			if(shift_motor==2'b00)begin
-				
-				////////////////mover manualmente motor phi
-				
-				//NO mover motor theta
-				s_out_theta_pos=2'b00;
-				s_out_theta_neg=2'b00;
+					////////////////mover automaticamente motor theta
 					
-				if(phi_actual>=(phi_manual+error)||phi_actual<=(phi_manual-error))begin 	
-					if(phi_actual>phi_manual) begin
-						if((phi_actual-phi_manual)<=giro) begin
-							s_out_phi_pos=2'b01;	// movimiento horario horizontal
-							s_out_phi_neg=2'b00;
-							
-						end else begin
-							s_out_phi_pos=2'b00;
-							s_out_phi_neg=2'b01; // movimiento anti-horario horizontal
-						end	
-					end else begin
-						if((phi_manual-phi_actual)<=giro) begin
-							s_out_phi_pos=2'b00;
-							s_out_phi_neg=2'b01; // movimiento anti-horario horizontal
-						end else begin
-							s_out_phi_pos=2'b01; // movimiento horario horizontal
-							s_out_phi_neg=2'b00;
-						end
-					end
-				end else begin 
-					shift_motor=2'b10;
 					//NO mover motor phi
-					s_out_phi_pos=2'b00;
-					s_out_phi_neg=2'b00;
-				end
-				////////////////mover manualmente motor phi
-			end else begin
-				
-				////////////////mover manualmente motor theta
-				
-				//NO mover motor phi
-				s_out_phi_pos=2'b00;
-				s_out_phi_neg=2'b00;
-				
-				if(theta_actual>=(theta_manual+error)||theta_actual<=(theta_manual-error))begin 
-					if(theta_actual>theta_manual) begin
-						s_out_theta_pos=2'b01; // movimiento horario vertical
-						s_out_theta_neg=2'b00; 
+					s_out_phi_pos=1;
+					s_out_phi_neg=1;
+		
+					if ((R_vertical_1>=(R_vertical_2-error) )&& (R_vertical_1<=(R_vertical_2+error)))begin
+						//NO mover motor theta
+						s_out_theta_neg=1;
+						s_out_theta_pos=1;
+						shift_motor=2'b10;
 					end else begin 
-						s_out_theta_pos=2'b00;
-						s_out_theta_neg=2'b01; // movimiento antihorario vertical
-					end	
-				end else begin 
-					shift_motor=2'b00;
+						if(R_vertical_1>R_vertical_2)begin
+							s_out_theta_pos=0; // movimiento horario vertical
+							s_out_theta_neg=1;
+						end
+						if(R_vertical_1<R_vertical_2)begin
+							s_out_theta_neg=0; // movimiento anti-horario vertical
+							s_out_theta_pos=1;
+						end	
+					end
+					////////////////mover automaticamente motor theta
+					
+				end else begin		
+				
+					////////////////mover automaticamente motor phi
+
 					//NO mover motor theta
-					s_out_theta_pos=2'b00;
-					s_out_theta_neg=2'b00;
+					s_out_theta_pos=1;
+					s_out_theta_neg=1;
+					
+					if (R_horizontal_1>=(R_horizontal_2-error) && (R_horizontal_1<=(R_horizontal_2+error)))begin
+							//NO mover motor phi
+
+							s_out_phi_pos=1;
+							s_out_phi_neg=1;
+							shift_motor=2'b00; 
+					end else begin 
+						if(R_horizontal_1>R_horizontal_2)begin
+							s_out_phi_pos=0; // movimiento horario  horizontal
+							s_out_phi_neg=1; 
+						end	
+						if(R_horizontal_1<R_horizontal_2)begin
+							s_out_phi_neg=0; // movimiento anti-horario horizontal
+							s_out_phi_pos=1;
+						end	
+					end
+					////////////////mover automaticamente motor phi
 				end
-				////////////////mover manualmente motor theta
-			end
-		//----------------------//MODO MANUAL//----------------------//
-		end	
+			//----------------------//MODO AUTOMATICO//----------------------//	
+			end else begin
+			//----------------------//MODO MANUAL//----------------------//
+				
+				if(shift_motor==2'b00)begin
+					
+					////////////////mover manualmente motor phi
+					
+					//NO mover motor theta
+					s_out_theta_pos=1;
+					s_out_theta_neg=1;
+						
+					if(phi_actual>=(phi_manual+error)||phi_actual<=(phi_manual-error))begin 	
+						if(phi_actual>phi_manual) begin
+							if((phi_actual-phi_manual)<=giro) begin
+								s_out_phi_pos=0;	// movimiento horario horizontal
+								s_out_phi_neg=1;
+								
+							end else begin
+								s_out_phi_pos=1;
+								s_out_phi_neg=0; // movimiento anti-horario horizontal
+							end	
+						end else begin
+							if((phi_manual-phi_actual)<=giro) begin
+								s_out_phi_pos=1;
+								s_out_phi_neg=0; // movimiento anti-horario horizontal
+							end else begin
+								s_out_phi_pos=0; // movimiento horario horizontal
+								s_out_phi_neg=1;
+							end
+						end
+					end else begin 
+						shift_motor=2'b10;
+						//NO mover motor phi
+						s_out_phi_pos=1;
+						s_out_phi_neg=1;
+					end
+					////////////////mover manualmente motor phi
+				end else begin
+					
+					////////////////mover manualmente motor theta
+					
+					//NO mover motor phi
+					s_out_phi_pos=1;
+					s_out_phi_neg=1;
+					
+					if(theta_actual>=(theta_manual+error)||theta_actual<=(theta_manual-error))begin 
+						if(theta_actual>theta_manual) begin
+							s_out_theta_pos=0; // movimiento horario vertical
+							s_out_theta_neg=1; 
+						end else begin 
+							s_out_theta_pos=1;
+							s_out_theta_neg=0; // movimiento antihorario vertical
+						end	
+					end else begin 
+						shift_motor=2'b00;
+						//NO mover motor theta
+						s_out_theta_pos=1;
+						s_out_theta_neg=1;
+					end
+					////////////////mover manualmente motor theta
+				end
+			//----------------------//MODO MANUAL//----------------------//
+			end	
+		end
 	end
-end
-endmodule
+endmodule 
